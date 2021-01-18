f7f0a2abab7c29b11800e97da2b4f38d44170e7c
Zareix <raphcatarino@gmail.com>
2021-01-18 23:45:40 +0100

    change Sping and background location

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Biographie from "../components/Biographie";
 import Competences from "../components/Competences";
 import Projets from "../components/Projets";
 import Contact from "../components/Contact";
+import { Spring, config } from "react-spring/renderprops";
 
 // styles
 const pageStyles = {
@@ -13,14 +14,33 @@ const pageStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 };
 
+const contentStyles = {
+  height: "1000px",
+  position: "relative",
+  top: -100,
+};
+
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <Layout>
         <Presentation />
-        <Biographie id={"bio"}/>
-        <Competences />
-        <Projets />
+        <Spring
+          from={{ opacity: 0 }}
+          to={{ opacity: 1 }}
+          config={config.molasses}
+        >
+          {(spring) => (
+            <div style={spring, contentStyles} className="row justify-content-center">
+              <div style={{borderRadius : "24px"}} className="col-10 p-0 bg-white">
+                <Biographie id={"bio"} />
+                <Competences />
+                <Projets />
+              </div>
+            </div>
+          )}
+        </Spring>
+
         <Contact />
       </Layout>
     </main>
