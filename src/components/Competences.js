411a8d7356363befccc12cf6ff19d39e580b1a64
Zareix <raphcatarino@gmail.com>
2021-01-19 18:13:58 +0100

    use of graphql and gatsby-image

--- src/components/Competences.js
+++ src/components/Competences.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React from "react"
+
+import { useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 const compTextStyles = {
   fontSize: "20px",
-};
+}
 
 const Competences = () => {
   // TODO : Use gatsby-image / graphql
-  function importAll(r) {
-    return r.keys().map(r);
-  }
-
-  const imagesWeb = importAll(
-    require.context("../images/logoComp/web", false, /\.(png|jpe?g|svg)$/)
-  );
-
-  const imagesDb = importAll(
-    require.context("../images/logoComp/database", false, /\.(png|jpe?g|svg)$/)
-  );
-
-  const imagesSoft = importAll(
-    require.context("../images/logoComp/logiciels", false, /\.(png|jpe?g|svg)$/)
-  );
+  const data = useStaticQuery(graphql`
+    query {
+      imagesWeb: allFile(filter: { relativeDirectory: { eq: "logoComp/web" } }) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      },
+      imagesSoft: allFile(filter: { relativeDirectory: { eq: "logoComp/logiciels" } }) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      },
+      imagesDb: allFile(filter: { relativeDirectory: { eq: "logoComp/database" } }) {
+        nodes {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  `)
 
   return (
     <div className="container">
@@ -33,43 +55,43 @@ const Competences = () => {
           <div className="col-12 p-4 justify-content-center text-center row">
             <h3 className="col-6 border-bottom">Web</h3>
             <div className="row col-12 justify-content-center">
-              {imagesWeb.map((img) => (
-                <img
-                  src={img}
-                  alt={`$(img) logo`}
-                  className="col-3 img-fluid"
-                ></img>
+              {data.imagesWeb.nodes.map((img) => (
+                <Img
+                  key={img.id}
+                  fluid={img.childImageSharp.fluid}
+                  className="col-3"
+                ></Img>
               ))}
             </div>
           </div>
           <div className="col-12 p-4 justify-content-center text-center row">
             <h3 className="col-6 border-bottom">Software</h3>
             <div className="row col-12 justify-content-center">
-              {imagesSoft.map((img) => (
-                <img
-                  src={img}
-                  alt={`$(img) logo`}
-                  className="col-3 img-fluid"
-                ></img>
-              ))}
-            </div>
-          </div>
-          <div className="col-12 p-4 justify-content-center text-center row">
-            <h3 className="col-6 border-bottom">Database</h3>
-            <div className="row col-12 justify-content-center">
-              {imagesDb.map((img) => (
-                <img
-                  src={img}
-                  alt={`$(img) logo`}
-                  className="col-3 img-fluid"
-                ></img>
+              {data.imagesSoft.nodes.map((img) => (
+                <Img
+                  key={img.id}
+                  fluid={img.childImageSharp.fluid}
+                  className="col-3"
+                ></Img>
               ))}
             </div>
           </div>
         </div>
+        <div className="col-12 p-4 justify-content-center text-center row">
+          <h3 className="col-6 border-bottom">Database</h3>
+          <div className="row col-12 justify-content-center">
+            {data.imagesDb.nodes.map((img) => (
+              <Img
+                key={img.id}
+                fluid={img.childImageSharp.fluid}
+                className="col-3"
+              ></Img>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Competences;
+export default Competences
