3b410bee956667cf73c4582943945873d4f738e8
Zareix <raphcatarino@gmail.com>
2021-01-20 18:31:04 +0100

    gatsby-background-image

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,25 +1,42 @@
 import React from "react"
 
-import bgImage from "../images/imgPres.jpg"
+import { useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 const jumboStyles = {
   height: "90vh",
   width: "100%",
-  backgroundImage: `url(${bgImage})`,
-  backgroundPosition: "center",
   margin: 0,
   color: "white",
 }
 
 const Presentation = () => {
   // TODO : Gatsby-background-image
+  const data = useStaticQuery(graphql`
+    query {
+      bgImage: file(relativePath: { eq: "imgPres.jpg" }) {
+        id
+        childImageSharp {
+          fluid (maxWidth : 1920) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <div style={jumboStyles} className="container-fluid row align-items-center">
+    <BackgroundImage
+      Tag="presentation"
+      fluid={data.bgImage.childImageSharp.fluid}
+      style={jumboStyles}
+      className="container-fluid row align-items-center"
+    >
       <div className="col text-center" style={{ top: -40 }}>
         <h1>Raphaël Catarino</h1>
         <h2>Etudiant en DUT Informatique</h2>
       </div>
-    </div>
+    </BackgroundImage>
   )
 }
 
