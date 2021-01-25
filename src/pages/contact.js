c12c7ca2e96109b9b7c6f6f5afbc37fb798fe442
Zareix <raphcatarino@gmail.com>
2021-01-25 19:22:27 +0100

    created contact page (to be dev)

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SEO from '../components/Seo'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <SEO title="Contact"/>
+        </div>
+    )
+}
+
+export default ContactPage
