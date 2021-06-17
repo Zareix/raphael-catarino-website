f242319c31a420bf4f7c8bc10823dda7c629c834
Zareix <raphcatarino@gmail.com>
2021-06-18 00:01:04 +0200

    change 404 svg name

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import "../styles/index.css"
-import OfflineSvg from "../images/svg/offline.svg"
+import Page404 from "../images/svg/page404.svg"
 
 import { Link } from "gatsby"
 import styled from "styled-components"
@@ -21,7 +21,7 @@ const NotFoundPage = () => {
   return (
     <MainStyled id="404">
       <Seo title="404" />
-      <OfflineSvg className="h-64 md:h-96" />
+      <Page404 className="h-64 md:h-96" />
       <h1 className="text-2xl">Oups ! Page introuvable. </h1>
       <Link to="/">
         <button className="px-3 py-2 rounded-md bg-blue-600 hover:bg-blue-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50">
