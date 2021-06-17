5e3be4c702bf236de4917adc15476953325a8a9b
Zareix <raphcatarino@gmail.com>
2021-06-17 22:57:16 +0200

    test forms

--- src/components/contact/ContactModal.js
+++ src/components/contact/ContactModal.js
@@ -28,7 +28,7 @@ function encode(data) {
 const ContactModal = (props) => {
   const { visible, close } = props
 
-  const [state, setState] = React.useState({})
+  const [state, setState] = useState({})
   const [sent, setSent] = useState(false)
 
   const handleChange = (e) => {
@@ -46,7 +46,10 @@ const ContactModal = (props) => {
         ...state,
       }),
     })
-      .then(() => console.log("sent"))
+      .then(() => {
+        setSent(true)
+        console.log("sent")
+      })
       .catch((error) => alert(error))
   }
 
@@ -57,81 +60,99 @@ const ContactModal = (props) => {
       style={modalStyles}
       onRequestClose={close}
     >
-      <form
-        className="flex w-full space-x-3"
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
+      {sent ? (
         <div className="w-full max-w-2xl px-5 py-10 m-auto mt-10 bg-white rounded-lg shadow dark:bg-gray-800">
-          <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
-            <p className="col-start-2 col-span-3">Contactez moi !</p>
-            <CgClose size={30} className="cursor-pointer" onClick={close} />
-          </div>
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
-            <div className="col-span-2 lg:col-span-1">
-              <div className=" relative ">
-                <input
-                  type="text"
-                  name="name"
-                  id="contact-form-name"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Votre nom"
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className="col-span-2 lg:col-span-1">
-              <div className=" relative ">
-                <input
-                  type="email"
-                  id="contact-form-email"
-                  name="email"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Votre email"
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className="col-span-2">
-              <div className=" relative ">
-                <input
-                  type="text"
-                  id="contact-form-subject"
-                  name="sujet"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Sujet"
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className="col-span-2">
-              <label className="text-gray-700" htmlFor="name">
-                <textarea
-                  className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  id="comment"
-                  placeholder="Que voulez-vous me dire ?"
-                  name="content"
-                  rows="5"
-                  cols="40"
-                  onChange={handleChange}
-                ></textarea>
-              </label>
-            </div>
-            <div className="col-span-2 text-right">
-              <button
-                type="submit"
-                className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-              >
-                Envoyer
-              </button>
-            </div>
+          <div className="mb-6 text-2xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
+            <CgClose
+              size={30}
+              className="cursor-pointer col-start-5"
+              onClick={close}
+            />
+            <p className="col-start-2 col-span-3 border-b border-gray-400 pb-2">
+              Message envoyé !
+            </p>
+            <p className="col-start-2 col-span-3 pt-2">
+              Je vous repondrai au plus vite
+            </p>
           </div>
         </div>
-      </form>
+      ) : (
+        <form
+          className="flex w-full space-x-3"
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          <div className="w-full max-w-2xl px-5 py-10 m-auto mt-10 bg-white rounded-lg shadow dark:bg-gray-800">
+            <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
+              <p className="col-start-2 col-span-3">Contactez moi</p>
+              <CgClose size={30} className="cursor-pointer" onClick={close} />
+            </div>
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
+              <div className="col-span-2 lg:col-span-1">
+                <div className=" relative ">
+                  <input
+                    type="text"
+                    name="name"
+                    id="contact-form-name"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Votre nom"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="col-span-2 lg:col-span-1">
+                <div className=" relative ">
+                  <input
+                    type="email"
+                    id="contact-form-email"
+                    name="email"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Votre email"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="col-span-2">
+                <div className=" relative ">
+                  <input
+                    type="text"
+                    id="contact-form-subject"
+                    name="sujet"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Sujet"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="col-span-2">
+                <label className="text-gray-700" htmlFor="name">
+                  <textarea
+                    className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    id="comment"
+                    placeholder="Que voulez-vous me dire ?"
+                    name="content"
+                    rows="5"
+                    cols="40"
+                    onChange={handleChange}
+                  ></textarea>
+                </label>
+              </div>
+              <div className="col-span-2 text-right">
+                <button
+                  type="submit"
+                  className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                >
+                  Envoyer
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      )}
     </Modal>
   )
 }
