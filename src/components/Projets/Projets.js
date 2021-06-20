dc63ce51924826ee82f3e707bb2599abfcca71d3
Zareix <raphcatarino@gmail.com>
2021-06-21 00:39:17 +0200

    ui fix

--- src/components/Projets/Projets.js
+++ src/components/Projets/Projets.js
@@ -8,7 +8,7 @@ import { FormattedMessage } from "react-intl"
 const Button = () => (
   <button
     type="button"
-    className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white font-semibold w-full transition ease-in duration-200 text-center text-sm shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+    className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold w-full transition ease-in duration-200 text-center text-sm shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 focus:ring-offset-indigo-200 rounded-lg "
   >
     <FormattedMessage id="projectLinkText" />
   </button>
