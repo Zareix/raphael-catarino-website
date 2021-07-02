557eeec4f1b6526cde563de6089f171a94faaeb4
Zareix <raphcatarino@gmail.com>
2021-07-02 18:11:27 +0200

    fix 404

---
+++ src/pages/404.js
@@ -0,0 +1,44 @@
+import React from "react"
+
+import "../styles/index.css"
+import Page404 from "../images/svg/page404.svg"
+
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Seo from "../components/Seo"
+
+const MainStyled = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+`
+
+const NotFoundPage = ({ data }) => {
+  return (
+    <MainStyled id="404">
+      <Seo title="404" />
+      <Page404 className="h-64 md:h-96" />
+      <h1 className="text-2xl">{data.datoCmsSiteConfig.notFoundMessage}</h1>
+      <Link to="/">
+        <button className="px-3 py-2 rounded-md bg-blue-600 hover:bg-blue-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50">
+          {data.datoCmsSiteConfig.notFoundButtonText}
+        </button>
+      </Link>
+    </MainStyled>
+  )
+}
+
+export default NotFoundPage
+
+export const query = graphql`
+  query pageNotFoundMain {
+    datoCmsSiteConfig(locale: { eq: "fr" }) {
+      notFoundMessage
+      notFoundButtonText
+    }
+  }
+`
