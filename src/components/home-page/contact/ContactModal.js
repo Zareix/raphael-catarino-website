77161bfe0152015eb9a445778040aa06a367d5f9
Zareix <raphcatarino@gmail.com>
2021-09-20 00:15:43 +0200

    added framer motion to modal

--- src/components/home-page/contact/ContactModal.js
+++ src/components/home-page/contact/ContactModal.js
@@ -2,25 +2,10 @@ import React, { useState } from "react"
 
 import { CgClose } from "@react-icons/all-files/cg/CgClose"
 import { useForm } from "react-hook-form"
-import styled, { keyframes } from "styled-components"
+import styled from "styled-components"
+import { AnimatePresence, motion } from "framer-motion"
 
-const slideInFromBottom = keyframes`
-  from {
-    transform: translateY(50px);
-    opacity : 0
-  }
-
-  to {
-    transform: translateY(0);
-    opacity: 1;
-  }
-`
-
-const AnimatedForm = styled.form`
-  animation: ${slideInFromBottom} 500ms ease;
-`
-
-const MyModal = styled.div`
+const MyModal = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
@@ -36,6 +21,48 @@ const MyModal = styled.div`
 const ErrorMessage = styled.span`
   color: #ef4444;
 `
+
+const slideForm = {
+  hidden: {
+    y: "-50%",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      type: "spring",
+      stiffness: 100,
+      damping: 15,
+    },
+  },
+  exit: {
+    y: "100%",
+    opacity: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+}
+
+const fadeIn = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      type: "spring",
+      stiffness: 100,
+      damping: 15,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+}
 
 const encode = (data) =>
   Object.keys(data)
@@ -78,115 +105,128 @@ const ContactModal = ({ data, visible, close }) => {
   }
 
   return (
-    visible && (
-      <MyModal contentLabel="Formulaire de contact" id="formModalContact">
-        {sent ? (
-          <div className="w-full max-w-2xl px-2 md:px-5 py-10 mx-3 md:m-auto bg-white rounded-lg shadow dark:bg-gray-800">
-            <div className="mb-6 text-2xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
-              <CgClose
-                size={30}
-                className="cursor-pointer col-start-5"
-                onClick={closeModal}
-              />
-              <p className="col-start-2 col-span-3 border-b border-gray-400 pb-2">
-                {data.contactSentTitle}
-              </p>
-              <p className="col-start-2 col-span-3 pt-2">
-                {data.contactSentSubtitle}
-              </p>
-            </div>
-          </div>
-        ) : (
-          <AnimatedForm
-            className="flex w-full space-x-3"
-            name="contact"
-            method="post"
-            action="/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            id="formContact"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="w-full max-w-2xl px-5 py-10 mx-3 md:m-auto bg-white rounded-lg shadow dark:bg-gray-800">
-              <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
-                <p className="col-start-2 col-span-3">{data.contactTitle}</p>
+    <AnimatePresence>
+      {visible && (
+        <MyModal
+          contentLabel="Formulaire de contact"
+          id="formModalContact"
+          variants={fadeIn}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          {sent ? (
+            <div className="w-full max-w-2xl px-2 md:px-5 py-10 mx-3 md:m-auto bg-white rounded-lg shadow dark:bg-gray-800">
+              <div className="mb-6 text-2xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
                 <CgClose
                   size={30}
-                  className="cursor-pointer"
+                  className="cursor-pointer col-start-5"
                   onClick={closeModal}
                 />
-              </div>
-              <input type="hidden" name="form-name" value="contact" />
-              <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
-                <div className="col-span-2 lg:col-span-1">
-                  <input
-                    type="text"
-                    name="nom"
-                    id="contactName"
-                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder={data.contactPlaceholderName}
-                    {...register("nom", { required: true })}
-                  />
-                  {errors.nom && (
-                    <ErrorMessage>{data.contactErreurNom}</ErrorMessage>
-                  )}
-                </div>
-                <div className="col-span-2 lg:col-span-1">
-                  <input
-                    type="email"
-                    id="contactEmail"
-                    name="email"
-                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder={data.contactPlaceholderEmail}
-                    {...register("email", { required: true })}
-                  />
-                  {errors.email && (
-                    <ErrorMessage>{data.contactErreurEmail}</ErrorMessage>
-                  )}
-                </div>
-                <div className="col-span-2">
-                  <input
-                    type="text"
-                    id="contactSubject"
-                    name="sujet"
-                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder={data.contactPlaceholderSujet}
-                    {...register("sujet", { required: true })}
-                  />
-                  {errors.sujet && (
-                    <ErrorMessage>{data.contactErreurSujet}</ErrorMessage>
-                  )}
-                </div>
-                <div className="col-span-2">
-                  <label className="text-gray-700" htmlFor="contenu">
-                    <textarea
-                      className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      id="contactContent"
-                      placeholder={data.contactPlaceholderContenu}
-                      name="contenu"
-                      rows="5"
-                      cols="40"
-                      {...register("contenu", { required: true })}
-                    ></textarea>
-                  </label>
-                  {errors.contenu && (
-                    <ErrorMessage>{data.contactErreurContenu}</ErrorMessage>
-                  )}
-                </div>
-                <div className="col-span-2 text-right">
-                  <button
-                    type="submit"
-                    className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                  >
-                    {data.contactSendBtn}
-                  </button>
-                </div>
+                <p className="col-start-2 col-span-3 border-b border-gray-400 pb-2">
+                  {data.contactSentTitle}
+                </p>
+                <p className="col-start-2 col-span-3 pt-2">
+                  {data.contactSentSubtitle}
+                </p>
               </div>
             </div>
-          </AnimatedForm>
-        )}
-      </MyModal>
-    )
+          ) : (
+            <motion.form
+              variants={slideForm}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              className="flex w-full space-x-3"
+              name="contact"
+              method="post"
+              action="/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              id="formContact"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="w-full max-w-2xl px-5 py-10 mx-3 md:m-auto bg-white rounded-lg shadow dark:bg-gray-800">
+                <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white grid grid-cols-5 justify-items-center">
+                  <p className="col-start-2 col-span-3">{data.contactTitle}</p>
+                  <CgClose
+                    size={30}
+                    className="cursor-pointer"
+                    onClick={closeModal}
+                  />
+                </div>
+                <input type="hidden" name="form-name" value="contact" />
+                <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
+                  <div className="col-span-2 lg:col-span-1">
+                    <input
+                      type="text"
+                      name="nom"
+                      id="contactName"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      placeholder={data.contactPlaceholderName}
+                      {...register("nom", { required: true })}
+                    />
+                    {errors.nom && (
+                      <ErrorMessage>{data.contactErreurNom}</ErrorMessage>
+                    )}
+                  </div>
+                  <div className="col-span-2 lg:col-span-1">
+                    <input
+                      type="email"
+                      id="contactEmail"
+                      name="email"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      placeholder={data.contactPlaceholderEmail}
+                      {...register("email", { required: true })}
+                    />
+                    {errors.email && (
+                      <ErrorMessage>{data.contactErreurEmail}</ErrorMessage>
+                    )}
+                  </div>
+                  <div className="col-span-2">
+                    <input
+                      type="text"
+                      id="contactSubject"
+                      name="sujet"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      placeholder={data.contactPlaceholderSujet}
+                      {...register("sujet", { required: true })}
+                    />
+                    {errors.sujet && (
+                      <ErrorMessage>{data.contactErreurSujet}</ErrorMessage>
+                    )}
+                  </div>
+                  <div className="col-span-2">
+                    <label className="text-gray-700" htmlFor="contenu">
+                      <textarea
+                        className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        id="contactContent"
+                        placeholder={data.contactPlaceholderContenu}
+                        name="contenu"
+                        rows="5"
+                        cols="40"
+                        {...register("contenu", { required: true })}
+                      ></textarea>
+                    </label>
+                    {errors.contenu && (
+                      <ErrorMessage>{data.contactErreurContenu}</ErrorMessage>
+                    )}
+                  </div>
+                  <div className="col-span-2 text-right">
+                    <button
+                      type="submit"
+                      className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    >
+                      {data.contactSendBtn}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </motion.form>
+          )}
+        </MyModal>
+      )}
+    </AnimatePresence>
   )
 }
 
