da5ecb0d1149110f57ea236d0fdba615bb430843
Zareix <raphcatarino@gmail.com>
2021-10-02 18:29:25 +0200

    rework nav on mobile

--- src/components/home-page/layout/navbar/Navbar.js
+++ src/components/home-page/layout/navbar/Navbar.js
@@ -13,15 +13,6 @@ import useScroll from "../../../hooks/use-scroll"
 import useWindowWidth from "../../../hooks/use-window-width"
 import { AnimatePresence, motion } from "framer-motion"
 
-const Overlay = styled.div`
-  position: fixed;
-  margin-top: -10px;
-  width: 100%;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.4);
-  backdrop-filter: blur(2px);
-`
-
 const slideIn = {
   hidden: {
     y: "-100%",
@@ -45,6 +36,33 @@ const slideIn = {
     },
   },
 }
+
+const fadeIn = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      type: "spring",
+      stiffness: 100,
+      damping: 15,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+}
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  margin-top: -10px;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  backdrop-filter: blur(2px);
+`
 
 const Navigation = ({
   data: {
@@ -227,75 +245,96 @@ const Navigation = ({
               </button>
             )}
           </div>
-          {isMobile && isNavDrawerOpen && (
-            <>
-              <div
-                id="navbarDrawer"
-                className="px-2 pt-2 pb-3 sm:px-3 bg-white dark:bg-gray-800 w-full fixed z-60"
-              >
-                <ul>
-                  {links.map((link, i) => (
-                    <li>
-                      {link.target.includes("/") ? (
-                        <GatsbyLink
-                          className="cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-                          activeClassName="link-active"
-                          to={
-                            (location.pathname.match(/(\/(..)\/)/)
-                              ? location.pathname
-                                  .match(/(\/(..)\/)/)[1]
-                                  .slice(0, -1)
-                              : "") + link.target
-                          }
-                          onClick={() => setTimeout(closeNavDrawer)}
-                        >
-                          {link.label}
-                        </GatsbyLink>
-                      ) : (
-                        <Link
-                          key={i}
-                          className="cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-                          to={link.target}
-                          activeClass="link-active"
-                          spy
-                          smooth
-                          offset={-70}
-                          duration={800}
-                          onClick={() => setTimeout(closeNavDrawer)}
-                        >
-                          {link.label}
-                        </Link>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-                {contactBtnVisible && (
-                  <button
-                    className="cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400  px-3 py-2 rounded-md text-base font-medium"
-                    onClick={openContactForm}
-                  >
-                    {labelContactLink}
-                  </button>
-                )}
-                {blogBtnVisible && (
-                  <GatsbyLink
-                    className="cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-                    to="blog"
-                  >
-                    {labelBlogLink}
-                  </GatsbyLink>
-                )}
-                <LangSelector
-                  mobile
-                  closeNavDrawer={closeNavDrawer}
-                  navVisible={visible || alwaysDisplayed}
-                  location={location}
-                  extSlug={langSlug}
+          <AnimatePresence>
+            {isMobile && isNavDrawerOpen && (
+              <>
+                <motion.div
+                  id="navbarDrawer"
+                  className="px-2 pt-8 pb-3 sm:px-3 bg-white dark:bg-gray-800 w-full fixed z-60 shadow-md"
+                  variants={slideIn}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                >
+                  <ul>
+                    {links.map((link, i) => (
+                      <li>
+                        {link.target.includes("/") ? (
+                          <GatsbyLink
+                            className="cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
+                            activeClassName="link-active"
+                            to={
+                              (location.pathname.match(/(\/(..)\/)/)
+                                ? location.pathname
+                                    .match(/(\/(..)\/)/)[1]
+                                    .slice(0, -1)
+                                : "") + link.target
+                            }
+                            onClick={() => setTimeout(closeNavDrawer)}
+                          >
+                            {link.label}
+                          </GatsbyLink>
+                        ) : (
+                          <Link
+                            key={i}
+                            className="cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
+                            to={link.target}
+                            activeClass="link-active"
+                            spy
+                            smooth
+                            offset={-70}
+                            duration={800}
+                            onClick={() => setTimeout(closeNavDrawer)}
+                          >
+                            {link.label}
+                          </Link>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="flex divide-x">
+                    {contactBtnVisible && (
+                      <button
+                        className={
+                          "cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400  p-2 rounded-md text-base font-medium" +
+                          (blogBtnVisible ? " w-1/2 text-right" : " mx-auto")
+                        }
+                        onClick={openContactForm}
+                      >
+                        {labelContactLink}
+                      </button>
+                    )}
+                    {blogBtnVisible && (
+                      <GatsbyLink
+                        className={
+                          "cursor-pointer text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 block p-2 rounded-md text-base font-medium" +
+                          (contactBtnVisible ? " w-1/2 text-left" : " mx-auto")
+                        }
+                        to="blog"
+                      >
+                        {labelBlogLink}
+                      </GatsbyLink>
+                    )}
+                  </div>
+                  <LangSelector
+                    mobile
+                    closeNavDrawer={closeNavDrawer}
+                    navVisible={visible || alwaysDisplayed}
+                    location={location}
+                    extSlug={langSlug}
+                  />
+                </motion.div>
+                <Overlay
+                  id="navbarOverlay"
+                  onClick={closeNavDrawer}
+                  variants={fadeIn}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
                 />
-              </div>
-              <Overlay id="navbarOverlay" onClick={closeNavDrawer} />
-            </>
-          )}
+              </>
+            )}
+          </AnimatePresence>
         </motion.nav>
       )}
     </AnimatePresence>
