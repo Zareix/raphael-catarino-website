6bf02cdca6dddd071e106eacc8cae8b30032ce19
Raphaël Catarino <raphcatarino@gmail.com>
2022-01-18 11:01:27 +0100

    update package and added auto light/dark switch mode

--- src/components/hooks/use-scroll-event.jsx
+++ src/components/hooks/use-scroll-event.jsx
@@ -1,37 +1,37 @@
 const useScrollEvent = () => {
-  var keys = { 37: 1, 38: 1, 39: 1, 40: 1 }
-
-  function preventDefault(e) {
-    e.preventDefault()
-  }
-
-  function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-      preventDefault(e)
-      return false
-    }
-  }
-
-  // modern Chrome requires { passive: false } when adding event
-  var supportsPassive = false
-  try {
-    window.addEventListener(
-      "test",
-      null,
-      Object.defineProperty({}, "passive", {
-        get: function () {
-          supportsPassive = true
-          return supportsPassive
-        },
-      })
-    )
-  } catch (e) {}
-
-  var wheelOpt = supportsPassive ? { passive: false } : false
-  var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel"
-
   // call this to Disable
   function disableScroll() {
+    let keys = { 37: 1, 38: 1, 39: 1, 40: 1 }
+
+    function preventDefault(e) {
+      e.preventDefault()
+    }
+
+    function preventDefaultForScrollKeys(e) {
+      if (keys[e.keyCode]) {
+        preventDefault(e)
+        return false
+      }
+    }
+
+    // modern Chrome requires { passive: false } when adding event
+    let supportsPassive = false
+    try {
+      window.addEventListener(
+        "test",
+        null,
+        Object.defineProperty({}, "passive", {
+          get: function () {
+            supportsPassive = true
+            return supportsPassive
+          },
+        })
+      )
+    } catch (e) {}
+
+    let wheelOpt = supportsPassive ? { passive: false } : false
+    let wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel"
+
     window.addEventListener("DOMMouseScroll", preventDefault, false) // older FF
     window.addEventListener(wheelEvent, preventDefault, wheelOpt) // modern desktop
     window.addEventListener("touchmove", preventDefault, wheelOpt) // mobile
@@ -40,6 +40,37 @@ const useScrollEvent = () => {
 
   // call this to Enable
   function enableScroll() {
+    let keys = { 37: 1, 38: 1, 39: 1, 40: 1 }
+
+    function preventDefault(e) {
+      e.preventDefault()
+    }
+
+    function preventDefaultForScrollKeys(e) {
+      if (keys[e.keyCode]) {
+        preventDefault(e)
+        return false
+      }
+    }
+
+    // modern Chrome requires { passive: false } when adding event
+    let supportsPassive = false
+    try {
+      window.addEventListener(
+        "test",
+        null,
+        Object.defineProperty({}, "passive", {
+          get: function () {
+            supportsPassive = true
+            return supportsPassive
+          },
+        })
+      )
+    } catch (e) {}
+
+    let wheelOpt = supportsPassive ? { passive: false } : false
+    let wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel"
+
     window.removeEventListener("DOMMouseScroll", preventDefault, false)
     window.removeEventListener(wheelEvent, preventDefault, wheelOpt)
     window.removeEventListener("touchmove", preventDefault, wheelOpt)
