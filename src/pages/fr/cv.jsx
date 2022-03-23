68cdfd6018490e1fc13c44168e50039292821182
Raphaël Catarino <raphcatarino@gmail.com>
2022-03-23 15:29:25 +0100

    loading

--- src/pages/fr/cv.jsx
+++ src/pages/fr/cv.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { graphql } from "gatsby";
 import styled from "styled-components";
 
 import CurriculumVitae from "../../components/cv/CurriculumVitae";
+import { Helmet } from "react-helmet";
+import { HelmetDatoCms } from "gatsby-source-datocms";
 
 const Wrapper = styled.div`
   -webkit-print-color-adjust: exact;
@@ -17,18 +19,28 @@ const Wrapper = styled.div`
 `;
 
 const CV = ({ data }) => {
+  const [loading, setLoading] = useState(false);
   const cvData = data.datoCmsCv;
 
   return (
-    <Wrapper>
-      <a
-        className="mx-auto mt-4 mb-2 rounded-lg bg-blue-700 px-3 py-2 text-white shadow-md transition-all hover:bg-blue-600 hover:shadow-lg print:hidden"
-        href={`${process.env.API_ADRESS}/cv-api?lang=fr`}
-      >
-        {cvData.downloadButtonText}
-      </a>
-      <CurriculumVitae data={cvData}></CurriculumVitae>
-    </Wrapper>
+    <>
+      <Helmet
+        htmlAttributes={{
+          lang: "fr",
+        }}
+      />
+      <HelmetDatoCms seo={cvData.seoMetaTags} />
+      <Wrapper>
+        <a
+          className="mx-auto mt-4 mb-2 rounded-lg bg-blue-700 px-3 py-2 text-white shadow-md transition-all hover:bg-blue-600 hover:shadow-lg print:hidden"
+          href={`${process.env.API_ADRESS}/cv-api?lang=fr`}
+          onClick={() => setLoading(true)}
+        >
+          {loading ? cvData.loadingButtonText : cvData.downloadButtonText}
+        </a>
+        <CurriculumVitae data={cvData}></CurriculumVitae>
+      </Wrapper>
+    </>
   );
 };
 
@@ -38,6 +50,7 @@ export const query = graphql`
   query CVFr {
     datoCmsCv(locale: { eq: "fr" }) {
       downloadButtonText
+      loadingButtonText
       ...CVFragment
     }
   }
