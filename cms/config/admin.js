a48aa6e556f8570b70a0b6f87bb479f2cc174f21
Raphaël Catarino <raphcatarino@gmail.com>
2022-08-09 18:25:42 +0200

    email and update

--- cms/config/admin.js
+++ cms/config/admin.js
@@ -1,8 +1,21 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
+  },
+  email: {
+    config: {
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "strapi@zrx.sh",
+        defaultReplyTo: "strapi@zrx.sh",
+        testAddress: "strapi@zrx.sh",
+      },
+    },
   },
 });
