1b9db370749268adc681d1527c29859e9c7b475a
Raphaël Catarino <raphcatarino@gmail.com>
2022-09-03 00:14:15 +0200

    cleanup + update

--- client/helpers/strapi.ts
+++ client/helpers/strapi.ts
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-import {
-  StrapiSingularResponse,
-  StrapiPluralResponse,
-} from '../models/StrapiResponse';
-
 const getStrapiMedia = (url: string): string => {
   return url.startsWith('/')
     ? process.env.NEXT_PUBLIC_STRAPI_PUBLIC_URL + url
