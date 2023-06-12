0e67b0e3efc026be60b200535e06777ee6c5b8eb
Raphaël Catarino <raphcatarino@gmail.com>
2023-06-12 23:07:28 +0200

    unlighthouse config

---
+++ unlighthouse.config.ts
@@ -0,0 +1,12 @@
+export default {
+  site: 'raphael-catarino.fr',
+  outputPath: 'unlighthouse',
+  scanner: {
+    samples: 3,
+  },
+  ci: {
+    budget: 70,
+    buildStatic: true,
+    reporter: 'jsonExpanded',
+  },
+};
