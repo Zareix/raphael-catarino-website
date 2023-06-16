0e1c91471d61028045be717b35e9569674e843cc
Raphaël Catarino <raphcatarino@gmail.com>
2023-06-16 19:48:26 +0200

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,26 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raphael-catarino.fr',
-  integrations: [tailwind(), prefetch({
-    selector: "a[href^='/']"
-  }), sitemap({
-    i18n: {
-      defaultLocale: 'en',
-      locales: {
-        en: 'en-US',
-        fr: 'fr-FR'
-      }
-    }
-  }), react()],
+  integrations: [
+    tailwind(),
+    prefetch({
+      selector: "a[href^='/']",
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          fr: 'fr-FR',
+        },
+      },
+    }),
+  ],
   experimental: {
-    assets: true
-  }
+    assets: true,
+  },
 });
