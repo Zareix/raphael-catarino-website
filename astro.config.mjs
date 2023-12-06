097754c55a0471134e04d085ce05bf70609bce09
Raphaël Catarino <raphcatarino@gmail.com>
2023-12-07 01:28:31 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
@@ -26,11 +26,11 @@ export default defineConfig({
   ],
   output: 'hybrid',
   adapter: cloudflare({
-    mode: "advanced",
+    mode: 'advanced',
     runtime: {
-     mode: 'local',
-     type: 'pages',
-    }
+      mode: 'local',
+      type: 'pages',
+    },
   }),
   image: {
     service: {
