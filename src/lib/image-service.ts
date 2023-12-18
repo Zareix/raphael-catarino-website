0ab50b0d1b01afb4d3f86e72bcfa773099ef1fb6
Raphaël Catarino <raphcatarino@gmail.com>
2023-12-18 02:38:16 +0200

    fix sharp

--- src/lib/image-service.ts
+++ src/lib/image-service.ts
@@ -1,14 +1,13 @@
 import type { LocalImageService } from 'astro';
 import { baseService } from 'astro/assets';
 
-// Waiting for https://github.com/oven-sh/bun/issues/4549 to replace with sharp
 const service: LocalImageService = {
   ...baseService,
 
   async transform(inputBuffer, transform, serviceConfig) {
     // Purposefully obfuscate the import to prevent bundling => will only work at build time!
     const imageService = (
-      await new Function(`return import('astro/assets/services/squoosh')`)()
+      await new Function(`return import('astro/assets/services/sharp')`)()
     ).default;
 
     return await imageService.transform(inputBuffer, transform, serviceConfig);
