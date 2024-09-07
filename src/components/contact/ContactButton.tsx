76958ace2d68f5972af128d4685dffe56d573a5d
Raphaël Catarino <raphcatarino@gmail.com>
2024-09-07 20:34:45 +0200

    remove some gsap

--- src/components/contact/ContactButton.tsx
+++ src/components/contact/ContactButton.tsx
@@ -62,7 +62,7 @@ const ContactButton = ({ lang }: Props) => {
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 z-40 bg-black/40 backdrop-blur" />
         <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 mx-[-1px] flex flex-col">
-          <div className="border-b-none mx-auto flex max-w-[600px] flex-1 flex-col rounded-t-[10px] border border-gray-200 bg-stone-50 p-4 md:px-12 dark:bg-slate-900">
+          <div className="border-b-none mx-auto flex max-w-[600px] flex-1 flex-col rounded-t-[10px] border border-gray-200 bg-stone-50 p-4 md:px-12 dark:border-slate-500 dark:bg-slate-900">
             <Drawer.Close
               className="mx-auto mb-4 h-1.5 w-12 flex-shrink-0 rounded-full bg-stone-300"
               aria-label="close"
--- src/components/home/skills/SkillsSelector.tsx
+++ src/components/home/skills/SkillsSelector.tsx
@@ -12,14 +12,14 @@ export const SkillsSelector = ({ domains }: SkillsSelectorProps) => {
   );
 
   return (
-    <section className="grid w-full gap-4">
-      <ul className="flex justify-center gap-2">
+    <div className="flex h-[250px] w-full flex-col gap-4">
+      <ul className="flex h-min justify-center gap-2">
         {Object.keys(domains).map((domain) => (
           <li
             key={domain}
             onMouseEnter={() => setSelectedDomainName(domain)}
             className={
-              'rounded-lg border border-gray-300 px-2 py-0.5 backdrop-blur-[2px] transition-colors duration-300 ease-out dark:border-slate-400' +
+              'h-min rounded-lg border border-gray-300 px-2 py-0.5 backdrop-blur-[2px] transition-colors duration-300 ease-out dark:border-slate-400' +
               (selectedDomainName === domain
                 ? ' bg-gray-300/30 dark:bg-slate-400/20'
                 : '')
@@ -54,6 +54,6 @@ export const SkillsSelector = ({ domains }: SkillsSelectorProps) => {
           </div>
         ))}
       </div>
-    </section>
+    </div>
   );
 };
