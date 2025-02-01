35b74e840367863221f3791f1a6575f2d52d10a9
Raphaël Catarino <raphcatarino@gmail.com>
2025-02-01 10:55:24 +0100

    update

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raphael-catarino.fr',
+
   prefetch: {
     prefetchAll: true,
   },
+
   i18n: {
     defaultLocale: 'en',
     locales: [
@@ -22,8 +24,8 @@ export default defineConfig({
       },
     ],
   },
+
   integrations: [
-    tailwind(),
     sitemap({
       i18n: {
         defaultLocale: 'en',
@@ -35,11 +37,16 @@ export default defineConfig({
     }),
     react(),
   ],
+
   redirects: {
     '/s/github': 'https://github.com/Zareix',
     '/s/instagram': 'https://www.instagram.com/raphaelgc',
     '/s/linkedin':
       'https://www.linkedin.com/in/rapha%C3%ABl-gon%C3%A7alves-catarino/',
     '/s/bento': 'https://bento.me/raphael-catarino',
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
--- src/components/home/hero/SkillsSubtitle.tsx
+++ src/components/home/hero/SkillsSubtitle.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DOMAINS } from '~/content/skills';
 
 import * as icons from '~/components/home/skills/Icons';
@@ -36,12 +36,12 @@ const SkillsSubtitle = ({ lang }: Props) => {
   }, []);
 
   return (
-    <h3 className="flex items-center gap-2 text-slate-700 dark:text-gray-300">
+    <h3 className="flex items-center gap-2 text-xl font-semibold text-slate-700 lg:text-2xl dark:text-gray-300">
       <div>{t('hero.subtitle-techno')}</div>
       <div
         id="heroTechno"
         className={
-          'flex items-center gap-1 bg-gradient-to-t from-purple-700 to-purple-950 to-[180%] bg-clip-text pr-2 italic text-transparent dark:from-slate-50 dark:to-slate-500 dark:to-100%' +
+          'flex items-center gap-1 bg-gradient-to-t from-purple-700 to-purple-950 to-[180%] bg-clip-text pr-2 text-transparent italic dark:from-slate-50 dark:to-slate-500 dark:to-100%' +
           (currentIndex !== undefined ? ' animate' : '')
         }
         key={currentIndex}
@@ -49,7 +49,7 @@ const SkillsSubtitle = ({ lang }: Props) => {
         {icons[domains[currentIndex ?? 0]]({
           className: 'h-6 w-6',
         })}
-        {domains[currentIndex ?? 0]}
+        {domains[currentIndex ?? 0].replace('CSharp', 'C#')}
       </div>
     </h3>
   );
